2b470dc31ebfee56b2c20dc223e34c2636e78d0e
Leslie Shea <leslieyshea@gmail.com>
2019-02-18 10:43:25 -0800

    created quests, linked to single quest pages

--- src/choose-quest.js
+++ src/choose-quest.js
@@ -1,3 +1,17 @@
+import allQuests from './quests.js';
+
 const themesNode = document.getElementById('themes');
 
-for(let i = 0; i < )
+
+for(let i = 0; i < allQuests.length; i++) {
+    const currentQuest = allQuests[i];
+    const li = document.createElement('li');
+    const link = document.createElement('a');
+
+    link.href = 'quest.html?name=' + encodeURIComponent(currentQuest.id);
+    link.textContent = currentQuest.title;
+
+    themesNode.appendChild(li);
+    li.appendChild(link);
+    
+}
---
+++ src/quests.js
@@ -0,0 +1,15 @@
+const insaneAsylumQuest = {
+    id: 'insane-asylum',
+    title: 'Fly Out of the Cuckoo\'s Nest',
+    description: ''
+};
+
+const bankQuest = {
+    id: 'bank-quest',
+    title: 'Break the Bank',
+    description: ''
+};
+
+const allQuests = [insaneAsylumQuest, bankQuest];
+
+export default allQuests;
