096dcef6b7a2f59b9e3662681119d80de55aeb23
Leslie Shea <leslieyshea@gmail.com>
2019-02-20 20:57:20 -0800

    added more description to the insane asylum description and choices

--- src/quests.js
+++ src/quests.js
@@ -8,9 +8,9 @@ import bankThreeChoices from './themes/bank/bank-scenario-3/choices.js';
 const insaneTheme = {
     id: 'insane-quest',
     title: 'Fly Out of the Cuckoo\'s Nest',
-    description0: 'You wake up in an insane asylum. You are chained to the bed. You want to escape. You look around you for anything to help you escape.',
-    description1: 'After breaking free from your handcuffs, you roam around the room and begin searching for a way to unlock the door. What do you do next?',
-    description2: 'You\'ve successfully escaped the room and are standing in the hallway, now you must escape the hospital.  What do you do...',
+    description0: 'You wake up to the pungent smell of rusted metal. You open your eyes, squinting in an attempt to sharpen the blurred environment around you. You glance around the room and try to lift your arm up. To your surprise and confusion, both of your arms are chained tighly to the bed with metal handcuffs. Your heart starts to beat quickly, and you feel yourself panicking. You\'re looking for any way to break yourself free. What is your tool of your choice?',
+    description1: 'After breaking free from your handcuffs, you dart around the room and frantically begin searching for a way to unlock the door. What do you do next?',
+    description2: 'You\'ve successfully broken out of the room and are standing in the hallway. Now you must overcome the challenge of escaping this very creepy insane asylum. Can you make it out?',
     scenarios: [insaneOneChoices, insaneTwoChoices, insaneThreeChoices],
     images: ['url(../assets/img/bed.jpg)', 'url(../assets/img/asylum.jpg)', 'url(../assets/img/hallway.jpg']
 };
@@ -18,7 +18,7 @@ const insaneTheme = {
 const bankTheme = {
     id: 'bank-quest',
     title: 'Break the Bank',
-    description0: 'You\'re feeling adventurous and decide you want to try and rob a bank. You brainstorm some methods to do so. Which option do you go with?',
+    description0: 'You\'re feeling adventurous and decide you want to experience the thrill of robbing a bank. You brainstorm some methods to do so. Which option do you go with?',
     description1: 'You successfully sneak into a bank and proceed to the vault. While in the vault, you trip an alarm for the gold safe. The vault slams shut, locking you inside, what do you do?',
     description2: 'You\'ve made your way out of the vault relatively unscathed. Adrenaline is rushing now as you should try to figure a way out of the bank before you are caught by the police.',
     scenarios: [bankOneChoices, bankTwoChoices, bankThreeChoices],
--- src/themes/insane/insane-scenario-1/choices.js
+++ src/themes/insane/insane-scenario-1/choices.js
@@ -1,8 +1,8 @@
 const needle = { 
     name: 'Needle',
     id: 'needle',
-    description: 'You notice a needle sitting on the nightstand beside your bed.',
-    result: 'Use the needle to pick the lock of the handcuffs. Smart thinking, gain 5 clever points',
+    description: 'You glance over at the bedside table and see that a needle has been left there. Odd, but could come in handy.',
+    result: 'Stretch your body as close to the edge of the bed as possible and barely grab the needle with your teeth. You lower it down to your hands and begin picking the keyhole with the needle tip. Smart thinking earns you 5 clever points.',
     hp: 0,
     cp: 5
 };
@@ -10,8 +10,8 @@ const needle = {
 const hammer = {
     name: 'Hammer',
     id: 'hammer',
-    description: 'Open the bedside drawer and find a reflex hammer.', 
-    result: 'You are able to successfully break the handcuffs off with the hammer.  But in the process, injure yourself and lose 10 health points.',
+    description: 'Use your feet to quietly open the top drawer in the bedside table to find a reflex hammer inside.', 
+    result: 'You are able to successfully break the handcuffs off with the hammer, but you injure yourself in the process and lose 10 health points.',
     hp: -10,
     cp: 0
 };
@@ -19,8 +19,8 @@ const hammer = {
 const bruteForce = {
     name: 'Brute Force',
     id: 'brute-force',
-    description: 'You start wondering how strong the handcuffs are against your strength',
-    result: 'You use all your power to brute force your cuffs free from the bed. Your adrenaline boosts your health by 10 points. ',
+    description: 'You think you can break free from the handcuffs by pulling on them as hard as you can.',
+    result: 'You are mighty strong! You\'re free from your handcuffs and this adrenaline rush boosts your health by 10 points.',
     hp: 10,
     cp: 0
 };
