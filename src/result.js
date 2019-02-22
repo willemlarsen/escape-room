f2bfb8f829e50cc354718c0dbd3ce00020b22b5b
Leslie Shea <leslieyshea@gmail.com>
2019-02-21 20:18:32 -0800

    started styling about page

--- src/result.js
+++ src/result.js
@@ -1,14 +1,16 @@
 import scoreDisplay from './score-display.js';
 
 const outcomeMessage = document.getElementById('outcome');
-const cpMessage = document.getElementById('cp-message');
+// const cpMessage = document.getElementById('cp-message');
 const buttonNode = document.getElementById('play-again');
+
 const json = window.localStorage.getItem('user');
 const user = JSON.parse(json);
+
 scoreDisplay(user);
 
 if(user.hp <= 0) {
-    outcomeMessage.textContent = 'You were caught, whomp whomp!';
+    outcomeMessage.textContent = 'Sorry, ' + user.name + ', you were caught! Whomp whomp!';
 }
 else {
     outcomeMessage.textContent = 'Congratulations, ' + user.name + ', you escaped!';
@@ -21,6 +23,7 @@ else {
 buttonNode.addEventListener('click', function() {
     user.hp = 50;
     user.cp = 0;
+
     const json = JSON.stringify(user);
     window.localStorage.setItem('user', json);
     
@@ -30,5 +33,4 @@ buttonNode.addEventListener('click', function() {
     else {        
         window.location = '../pages/choose-theme.html';
     }
-    
 });
--- src/themes/bank/bank-scenario-1/choices.js
+++ src/themes/bank/bank-scenario-1/choices.js
@@ -2,7 +2,7 @@ const walk = {
     name: 'Walk In',
     id: 'walk',
     description: 'Casually stroll into the bank and head to the clerk\'s desk with your Nerf gun.',
-    result: 'You demand to go to the vault and to your suprise the clerk nervously agrees. Did they not realize that you\'re carrying a children\'s toy? 10 points for being clever... and super lucky.',
+    result: 'You demand to go to the vault, and to your suprise, the clerk nervously agrees. Did they not realize that you\'re carrying a children\'s toy? 10 points for being clever... and super lucky.',
     hp: 0,
     cp: 10
 };
@@ -10,8 +10,8 @@ const walk = {
 const drill = {
     name: 'Drill',
     id: 'drill',
-    description: 'Drill a human-sized hole in the wall leading straight into the vault.', 
-    result: 'Successfully sneak into the vault undetected. Earn 10 clever points, but minus 5 health points for having to re-drill the hole because you didn\'t fit the first time.',
+    description: 'Make a human-sized hole in the wall leading straight into the vault.', 
+    result: 'Successfully sneak into the vault undetected. Earn 10 clever points, but minus 5 health points for having to re-drill the hole because you didn\'t fit on your first attempt.',
     hp: -5,
     cp: 10
 };
